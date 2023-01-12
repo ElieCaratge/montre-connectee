c6cbf5d2ee477a59aac7b6128825adffc10360e9
Élie caratgé <elie.caratge@student-cs.fr>
2023-01-12 17:40:08 +0100

    Update des calculs Paramètres pour la génération aléatoire de patients

--- Patient.py
+++ Patient.py
@@ -8,12 +8,23 @@ class Patient:
     EXPERT_THRESHOLD = 7
 
     @classmethod
-    def generate_random_patient(cls):
+    def generate_random_patient(cls, freq=3, nb_seances=8, min_min=30, min_max=60):
+        """Générer un patient aléatoire.
+        
+        Paramètres
+        ----------
+        - freq (int) : Nombre de jours espacant chaque pratique sportive.
+        - nb_sports (int) : Nombre de séances de sports pratiquées.
+        - min_min (int) : Durée minimale d'une séance de sport.
+        - min_max (int) : Durée maximale d'une séance de sport.
+        """
+
         today = datetime.date.today()
         BPM = np.random.normal(70, 7, size=50)
         practice = [
-            (SPORTS_LIST[np.random.randint(N_SPORTS)], today-datetime.timedelta(3*i), datetime.timedelta(minutes=np.random.randint(20, 40))) for i in range(8)
+            (SPORTS_LIST[np.random.randint(N_SPORTS)], today-datetime.timedelta(freq*i), datetime.timedelta(minutes=np.random.randint(min_min, min_max))) for i in range(nb_seances)
         ]
+        
         return Patient(BPM, practice)
 
     def __init__(self, BPM, practice) -> None:
@@ -27,22 +38,32 @@ class Patient:
         return f"Patient({self.category})"
     
     def evaluate(self):
-        # Paramters to adjust
-        ALPHA, BETA = 70, 0.1
-        min_eq = self.process_activity().total_seconds()//60
+        """Évaluation de l'activité sportive du patient (note sur 10)"""
+
+        # Paramètres à ajuster
+        ALPHA, BETA = 70, 14
+        # Traitement des données
+        heq = self.process_activity()
         m_BPM = self.process_BPM()
-        return ALPHA/m_BPM + BETA*min_eq # Sould be between 0 and 10
+
+        return ALPHA/m_BPM + BETA*heq
 
     def process_activity(self):
-        """Return the number of equivalent hours of sport"""
+        """Nombre d'heures équivalentes de sport par semaine."""
+
         sum=datetime.timedelta()
+        first_activity = self.practice[0][1]
+
         for sport, date, duration in self.practice:
             sum += sport.equiv_hours(duration)
-        return sum
+            if date < first_activity:
+                first_activity = date
+
+        return (sum/(datetime.date.today()-first_activity).days*7).total_seconds()/3600
 
     def process_BPM(self):
-        """Return the mean of the BPM"""
+        """Rythme cardiaque moyen"""
+
         return np.mean(self.BPM)
 
 myPatient = Patient.generate_random_patient()
-print(myPatient.evaluation)
