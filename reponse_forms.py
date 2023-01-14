cb42832d8c2b1a1f17ca9de0ed1c55d735bb782c
Montet Benoit <benoit.montet@student-cs.fr>
2023-01-14 11:58:25 +0100

    Fichier Data avec dedans les données csv

--- reponse_forms.py
+++ reponse_forms.py
@@ -2,10 +2,12 @@ import csv
 import sklearn.cluster
 import pandas as pd
 import numpy as np
+from Affinity import feedback
+from Affinity import affinity_zero
 
 reponses = []
 # Read the csv file, mine is called reponses.csv
-with open('reponses.csv', newline='') as csvfile:
+with open('montre-connectee/Data/reponses.csv', newline='') as csvfile:
     reponse = csv.reader(csvfile)
     for row in reponse:
         # row[0] is a row with the date, we don't need it
@@ -13,34 +15,12 @@ with open('reponses.csv', newline='') as csvfile:
         reponses.append(row)
 
 sports = reponses.pop(0)
-# Extract sports
-affinités = np.zeros([len(sports), len(sports)])
 
-# We define an affinity treshold
-palier_affinité = 1
-
-for j in range(len(sports)):
-    for k in range(j+1):
-        m = 0
-        n = 0
-        for i in range(len(reponses)):
-            if (int(reponses[i][j]) >= palier_affinité and int(reponses[i][k]) >= 5) or (int(reponses[i][k]) >= palier_affinité and int(reponses[i][j]) >= 5):
-                # Calculate the difference for each user between sports j and k and make a mean
-                m += abs(int(reponses[i][j])-int(reponses[i][k]))
-                n += 1
-        if n != 0:
-            m_moy = m/n
-            # The greater the difference, the less affinity there is
-            m_prct = 100 - m_moy*10
-            affinités[j][k] = int(m_prct)
-            affinités[k][j] = int(m_prct)
-        else:
-            affinités[j][k] = None
-            affinités[k][j] = None
+affinités = affinity_zero
 
 # We compile in a panda dataframe the affinity between sports
-df = pd.DataFrame(data=affinités, index=sports, columns=sports)
-print(df)
+affinity_dataframe = pd.DataFrame(data=affinités, index=sports, columns=sports)
+print(affinity_dataframe)
 
 # We don't need to recommandate sports to everyone who answer to the google forms,
 # so we need a list with only the sports that users practise and like
@@ -54,7 +34,7 @@ sport_numéro = {sports[i]: i for i in range(len(sports))}
 
 fb = []
 
-with open('feedback.csv', newline='') as csvfile:
+with open('montre-connectee/Data/feedback.csv', newline='') as csvfile:
     feedback_csv = csv.reader(csvfile)
     for row in feedback_csv:
         # row[0] is a row with the date, we don't need it
@@ -62,22 +42,22 @@ with open('feedback.csv', newline='') as csvfile:
         fb.append(row)
 fb.pop(0)
 # Each line k is a recommandation rate of the sport k for the sport j (column)
-feedback = np.zeros([len(sports), len(sports)]).tolist()
-for i in range(len(feedback)):
-    for j in range(len(feedback[i])):
-        feedback[i][j] = [None]
+feedback_users = np.zeros([len(sports), len(sports)]).tolist()
+for i in range(len(feedback_users)):
+    for j in range(len(feedback_users[i])):
+        feedback_users[i][j] = [None]
 for feed in fb:
-    if feedback[sport_numéro[feed[0]]][sport_numéro[feed[1]]] != [None]:
-        feedback[sport_numéro[feed[0]]
-                 ][sport_numéro[feed[1]]].append(int(feed[2]))
+    if feedback_users[sport_numéro[feed[0]]][sport_numéro[feed[1]]] != [None]:
+        feedback_users[sport_numéro[feed[0]]
+                       ][sport_numéro[feed[1]]].append(int(feed[2]))
     else:
-        feedback[sport_numéro[feed[0]]
-                 ][sport_numéro[feed[1]]] = [int(feed[2])]
-for i in range(len(feedback)):
-    for j in range(len(feedback[i])):
-        if feedback[i][j] != None:
-            feedback[i][j] = sum(feedback[i][j])/len(feedback[i][j])
-print(feedback)
+        feedback_users[sport_numéro[feed[0]]
+                       ][sport_numéro[feed[1]]] = [int(feed[2])]
+for i in range(len(feedback_users)):
+    for j in range(len(feedback_users[i])):
+        if feedback_users[i][j] != None:
+            feedback_users[i][j] = sum(
+                feedback_users[i][j])/len(feedback_users[i][j])
 
 
 # Define a recommandation treshold ∈ [0,10] (here I took 5)
@@ -97,8 +77,8 @@ for i in range(len(users_sports)):
             # affinity between j and k (∈[0,1]) * how much the user i likes the sport k that is greater than the treshold
             # That product will be the recommendation rate
 
-            m = max(w*int(users_sports[i][j])*affinités[j]
-                    [k]/100 + (1-w)*feedback[k][j]*10, m)
+            m = max(int(users_sports[i][j])*(w*affinités[j]
+                    [k]/100 + (1-w)*feedback_users[k][j]), m)
 
         if m >= palier_recom:
             if empty:
@@ -150,8 +130,9 @@ for j in range(len(users_sports)):
         nb_recomm_max = len(user_recom[j])
     recommandations = ['recommandation ' +
                        str(i+1) for i in range(nb_recomm_max)]
-df = pd.DataFrame(data=user_recom, index=users, columns=recommandations)
-print(df)
+affinity_dataframe = pd.DataFrame(
+    data=user_recom, index=users, columns=recommandations)
+print(affinity_dataframe)
 
 # clusters = sklearn.cluster.k_means(users_sports, n_clusters=4)
 # print(clusters)
@@ -180,8 +161,9 @@ print(df)
 #                               7.50000000e-01,  3.75000000e-01,  1.12500000e+00,  4.25000000e+00,
 #                               1.25000000e+00,  8.75000000e-01,  0.00000000e+00]
 
+
 mets = []
-with open('METS.csv', newline='') as csvfile:
+with open('montre-connectee/Data/METS.csv', newline='') as csvfile:
     mets_ = csv.reader(csvfile)
     for row in mets_:
         mets.append(row)
