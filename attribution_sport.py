94a642be389d10ee967776ceb626a3d45c5f4f58
Alexandre Caillot <alexandre.caillot@student-cs.fr>
2023-01-19 15:34:24 +0100

    debug 1

--- attribution_sport.py
+++ attribution_sport.py
@@ -1,10 +1,9 @@
 import numpy as np
 import random
 
-
 def initialisation_probas(matrice_sports, sport_initial):
     """matrice_sports est un numpy array, sport_inital est un int lié à la liste des sports du fichier sports.py
-    renvoie des zones de probabilités entre 0 et 1 d'être le sport choisi"""
+    renvoie des plages de valeur entre 0 et 1 qui correspond à la probabilité de chaque sport d'être choisi"""
     array_probas = np.array([])
     nb_sports = np.shape(matrice_sports)[0]
     for i in range(nb_sports):
@@ -16,6 +15,7 @@ def initialisation_probas(matrice_sports, sport_initial):
 
 
 def attribution_sport(array_probas, liste_sports_restants):
+    """on choisit un nombre aléatoire et on regarde quel sport a été sélectionné"""
     x = random.random()
     i = 0
     while array_probas[i] < x:
@@ -24,13 +24,18 @@ def attribution_sport(array_probas, liste_sports_restants):
 
 
 def modification_liste_sports(array_probas, sport_choisi, rang_sport_choisi, liste_sports_restants, sports_deja_choisis):
+    "On rajoute le sport sélectionné à la liste des sports déjà choisis et on l'enlève de la liste des sports disponibles"
     sports_deja_choisis.append(sport_choisi)
     liste_sports_restants = np.delete(liste_sports_restants, rang_sport_choisi)
+    "On enlève la plage de valeur qui correspond au sport choisi"
+    if (rang_sport_choisi == 0):
+        proba_enlevee = array_probas[rang_sport_choisi]
+    else:
+        proba_enlevee = array_probas[rang_sport_choisi] - array_probas[rang_sport_choisi-1]
     for i in range(rang_sport_choisi, len(array_probas)):
-        proba_enlevee = array_probas[rang_sport_choisi +
-                                     1] - array_probas[rang_sport_choisi]
         array_probas[i] = array_probas[i] - proba_enlevee
-        np.delete(array_probas, rang_sport_choisi)
+    np.delete(array_probas, rang_sport_choisi)
+    "On renormalise la plage de valeurs"
     array_probas = array_probas/(1-proba_enlevee)
     return(array_probas, sports_deja_choisis, liste_sports_restants)
 
@@ -51,3 +56,4 @@ def fct_principale(matrice_sports, sport_initial):
         print(sports_deja_choisis, liste_sports_restants)
     sports_deja_choisis.append(sport_choisi)
     return(sports_deja_choisis)
+
