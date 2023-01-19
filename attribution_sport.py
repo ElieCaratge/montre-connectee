1df6387e940b3db77681ea36e2a23555cc4888af
Alexandre Caillot <alexandre.caillot@student-cs.fr>
2023-01-19 11:51:15 +0100

    update algo

--- attribution_sport.py
+++ attribution_sport.py
@@ -1,14 +1,16 @@
 import numpy as np
 import random
 
+
 def initialisation_probas(matrice_sports, sport_initial):
     """matrice_sports est un numpy array, sport_inital est un int lié à la liste des sports du fichier sports.py
     renvoie des zones de probabilités entre 0 et 1 d'être le sport choisi"""
     array_probas = np.array([])
     nb_sports = np.shape(matrice_sports)[0]
     for i in range(nb_sports):
-        if i != sport_initial:
-            array_probas = np.append(array_probas, matrice_sports[sport_initial][i])
+        if i != (sport_initial-1):
+            array_probas = np.append(
+                array_probas, matrice_sports[sport_initial-1][i])
     array_probas = np.cumsum(array_probas)/array_probas.sum()
     return(array_probas)
 
@@ -23,9 +25,11 @@ def attribution_sport(array_probas, liste_sports_restants):
 
 def modification_liste_sports(array_probas, sport_choisi, rang_sport_choisi, liste_sports_restants, sports_deja_choisis):
     sports_deja_choisis.append(sport_choisi)
-    liste_sports_restants = np.delete(liste_sports_restants, rang_sport_choisi-1)
+    liste_sports_restants = np.delete(
+        liste_sports_restants, rang_sport_choisi-1)
     for i in range(rang_sport_choisi, len(array_probas)):
-        proba_enlevee = array_probas[rang_sport_choisi + 1] - array_probas[rang_sport_choisi]
+        proba_enlevee = array_probas[rang_sport_choisi +
+                                     1] - array_probas[rang_sport_choisi]
         array_probas[i] = array_probas[i] - proba_enlevee
         np.delete(array_probas, rang_sport_choisi)
     array_probas = array_probas/(1-proba_enlevee)
@@ -38,10 +42,12 @@ def fct_principale(matrice_sports, sport_initial):
     liste_sports_restants = np.array([i for i in range(1, nb_sports+1)])
     liste_sports_restants = np.delete(liste_sports_restants, sport_initial)
     array_probas = initialisation_probas(matrice_sports, sport_initial)
-    (rang_sport_choisi, sport_choisi) = attribution_sport(array_probas, liste_sports_restants)
+    (rang_sport_choisi, sport_choisi) = attribution_sport(
+        array_probas, liste_sports_restants)
     while len(liste_sports_restants) > 1:
         (array_probas, sport_deja_choisis, liste_sports_restants) = modification_liste_sports(
             array_probas, sport_choisi, rang_sport_choisi, liste_sports_restants, sports_deja_choisis)
-        (rang_sport_choisi, sport_choisi) = attribution_sport(array_probas, liste_sports_restants)
+        (rang_sport_choisi, sport_choisi) = attribution_sport(
+            array_probas, liste_sports_restants)
     sports_deja_choisis.append(sport_choisi)
     return(sports_deja_choisis)
