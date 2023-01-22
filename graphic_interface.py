ff67290fc6fa735cff3ca308f66be2b8a4450ded
Escoubas Baptiste <baptiste.escoubas@student-cs.fr>
2023-01-22 17:27:50 +0100

    interface

--- graphic_interface.py
+++ graphic_interface.py
@@ -1,40 +1,27 @@
 from tkinter import *
 from functions import *
 from tkinter import ttk
+from datetime import *
 
 #création de fenêtre
 window=Tk()
 window.title('My Sport Coach')
 window.geometry('1080x720')
 
-#Création du widget à onglets
-tabControl = ttk.Notebook(window)
-
-#Création des frame qui vont servir d'onglets
-Recom = Frame(tabControl)
-Histo = Frame(tabControl)
-
-#Ajout du nom de ces onglets
-tabControl.add(Recom, text ='Recommandation')
-tabControl.add(Histo, text ='Historique')
-tabControl.pack(expand = 1, fill ="both")
-
-#Sous-frame des onglets (pour pouvoir mieux les gérer)
-sub_recom = Frame(Recom)
-sub_histo = Frame(Histo)
+#Création de la frame
+frame = Frame(window)
 
 #création des frame droite et gauche dans recommandation
-
-left_frame=Frame(sub_recom)
-right_frame = Frame(sub_recom)
+left_frame=Frame(frame)
+right_frame = Frame(frame)
 
 #Ajout d'un texte fixe dans l'onglet recommandation
 Our_recom = Label(left_frame,text='Notre recommandation :',font=('Arial',30))
 Our_recom.pack(pady=10)
 
 #Sport et durée arbitraires
-sport = ['Cyclisme','Tennis','Danse']
-time = [30,50,20]
+sport = ['Cyclisme','Tennis','Danse', 'Course à pied', 'Boxe']
+time = [90,60,45,40,80]
 
 #commande pour vider la frame de gauche
 def clear_frame():
@@ -116,19 +103,12 @@ short_button = Button(right_frame,text = "C'est trop court", font=('Arial',30),w
 short_button.pack(pady = 15, padx= 40)
 
 
-#Ajout d'un texte dans l'onglet Historique
-Your_Histo = Label(sub_histo,text='Votre Historique',font=('Arial',30))
-Your_Histo.pack()
-
 #On choisit l'emplacement des frame droite et gauche
 right_frame.grid(row=0, column = 1)
 left_frame.grid(row=0, column = 0)
 
-# On affiche ces sous-frame
-sub_histo.pack(expand=YES)
-sub_recom.pack(expand=YES)
-
-
+#On affiche notre frame 
+frame.pack(expand=YES)
 
 #Bouclage pour que l'interface tourne
 window.mainloop()
